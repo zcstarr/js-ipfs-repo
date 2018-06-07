faacb306d4129bbf3bc96ac1fafab4de46de46ae
Jacob Heun <jacobheun@gmail.com>
2018-06-07 20:13:48 +0200

    feat: add swarm key getter method
    License: MIT
    Signed-off-by: Jacob Heun <jacobheun@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const path = require('path')
 const debug = require('debug')
 const Big = require('big.js')
 const pull = require('pull-stream')
+const Key = require('interface-datastore').Key
 
 const backends = require('./backends')
 const version = require('./version')
@@ -335,6 +336,22 @@ class IpfsRepo {
         numObjects: results.blocks.count,
         repoSize: size
       })
+    })
+  }
+
+  /**
+   * Gets the swarm.key buffer, if it exists
+   *
+   * @param {function(Error, Buffer)} callback
+   * @returns {void}
+   */
+  swarmKey (callback) {
+    const swarmKeyKey = new Key('swarm.key')
+    this.root.get(swarmKeyKey, (err, swarmKeyBuffer) => {
+      if (err) {
+        return callback(new Error(ERRORS.SWARM_KEY_NOT_FOUND))
+      }
+      callback(null, swarmKeyBuffer)
     })
   }
 }
